1f2d0d3313c415a17e3c3e9c74aaef5b0c6e466a
garbled1 <root@garbled.net>
2020-11-28 06:13:06 -0700

    Add dry_run, minor cleanup, don't publish the last element because it's a partial.

--- venstar_influx/venstar_influx.py
+++ venstar_influx/venstar_influx.py
@@ -37,6 +37,9 @@ def parse_cmdline():
     parser.add_argument('-m', '--max_days', type=int, action='store',
                         default=99999999,
                         help='Maximum number of days to look back in the data')
+    parser.add_argument('-D', '--dry_run', action='store_true',
+                        default=False,
+                        help='Dry-Run, no datapoints will be written')
 
     args = parser.parse_args()
     return args
@@ -53,17 +56,20 @@ def run_notify(venstar_name):
         r_url = result.stdout.decode("utf-8")
         notify_url = r_url.rstrip()
         i += 1
-        
-    print("Notify returned url after {1} tries: {0}".format(notify_url,i))
+
+    print("Notify returned url after {1} tries: {0}".format(notify_url, i))
     return(notify_url)
 
 
 def parse_json(json_data, max_days):
     now_sec = int(time.time())
-    
+
     json_body = []
     for dp in json_data['runtimes']:
-        fields = {i:dp[i] for i in dp if i != 'ts'}
+        # skip last element
+        if dp == json_data['runtimes'][-1]:
+            continue
+        fields = {i: dp[i] for i in dp if i != 'ts'}
         point = {
             "measurement": "venstar_runtime",
             "time": dp['ts'],
@@ -72,7 +78,7 @@ def parse_json(json_data, max_days):
         if dp['ts'] > (now_sec - max_days * 86400):
             json_body.append(point)
     return(json_body)
-    
+
 
 def main():
     args = parse_cmdline()
@@ -95,7 +101,7 @@ def main():
     except:
         print("Cannot find db named {0}, please create".format(args.influx_db))
         exit(1)
-    
+
     if args.file is not None:
         if args.venstar_name is None:
             venstar_name = 'MainSouth'
@@ -104,11 +110,12 @@ def main():
         json_file = open(args.file)
         json_data = json.load(json_file)
         json_body = parse_json(json_data, args.max_days)
-        tags = { "name": venstar_name }
-        db_client.write_points(json_body, tags=tags, time_precision='s')
-        
+        tags = {"name": venstar_name}
+        if not args.dry_run:
+            db_client.write_points(json_body, tags=tags, time_precision='s')
+
         exit(0)
-    
+
     if args.venstar_url is None:
         if args.venstar_name is None:
             print("ERROR: Need either venstar URL or name")
@@ -151,11 +158,17 @@ def main():
         exit(1)
 
     json_body = parse_json(runtime_data, args.max_days)
-    tags = { "name": venstar_name }
-    try:
-        db_client.write_points(json_body, tags=tags, time_precision='s')
-    except Exception as e:
-        print("Cannot contact influx: {0}".format(str(e)))
+    tags = {"name": venstar_name}
+    if args.dry_run:
+        print("Would send to influx:")
+        print(json.dumps(json_body, indent=2))
+        print("Tags:")
+        print(tags)
+    else:
+        try:
+            db_client.write_points(json_body, tags=tags, time_precision='s')
+        except Exception as e:
+            print("Cannot contact influx: {0}".format(str(e)))
     exit(0)
 
 main()
